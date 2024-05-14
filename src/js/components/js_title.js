d576438530a04ecf027db651099b1c641835d4ea
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-05-14 12:55:32 +0300

    add title opacity on scroll

--- src/js/components/js_title.js
+++ src/js/components/js_title.js
@@ -3,27 +3,40 @@ export default function () {
     const title = parallax.querySelector('.js_title');
 
     const SPEED = 2;
+    const OPACITY = -0.2;
 
-    // высота всего параллакса
+    // total parallax height
     const parallaxTotalHeight = parallax.scrollHeight;
-    console.log('parallaxTotalHeight', parallaxTotalHeight);
+    // console.log('parallaxTotalHeight', parallaxTotalHeight);
 
-    // видимая высота параллакса
+    // console.log('parallax.getBoundingClientRect()', parallax.getBoundingClientRect());
+
+    // viewable parallax height
     const parallaxHeight = parallax.getBoundingClientRect().height.toFixed(0);
-    console.log('parallaxHeight', parallaxHeight);
+    // console.log('parallaxHeight', parallaxHeight);
 
-    // сколько осталось прокрутить до конца параллакса
+    // how much till the parallax end
     const restOfParallax = parallaxTotalHeight - parallaxHeight;
-    console.log('restOfParallax', restOfParallax);
+    // console.log('restOfParallax', restOfParallax);
 
     function changeTitlePos() {
-        // на сколько прокрутили
-        console.log('parallax.scrollTop', parallax.scrollTop);
+        // how much we scroll
+        // console.log('parallax.scrollTop', parallax.scrollTop);
 
         const titleTransformY = (parallax.scrollTop / restOfParallax).toFixed(3) * 100 * SPEED;
-        console.log('titleTransformY', titleTransformY);
+        // console.log('titleTransformY', titleTransformY);
 
-        title.setAttribute('style', `transform: translateY(${titleTransformY}%);`);
+        const titleOpacity = 1 - (parallax.scrollTop / restOfParallax).toFixed(1) + OPACITY;
+        // console.log('titleOpacity', titleOpacity);
+
+        title.setAttribute(
+            'style',
+            `transform: translateY(${titleTransformY}%); opacity: ${titleOpacity};`
+        );
+
+        if (parallax.scrollTop === 0) {
+            title.style.opacity = 1;
+        }
     }
 
     parallax.onscroll = changeTitlePos;
