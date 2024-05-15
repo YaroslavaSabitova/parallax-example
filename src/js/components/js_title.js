86d5c72b97ccd5280914ac3f973585f81cba34a4
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-05-15 17:17:29 +0300

    fix title js

--- src/js/components/js_title.js
+++ src/js/components/js_title.js
@@ -2,32 +2,20 @@ export default function () {
     const parallax = document.querySelector('.js_parallax');
     const title = parallax.querySelector('.js_title');
 
-    const SPEED = 2;
+    const SPEED = 1.5;
     const OPACITY = -0.2;
-
-    // total parallax height
-    const parallaxTotalHeight = parallax.scrollHeight;
-    // console.log('parallaxTotalHeight', parallaxTotalHeight);
-
-    // console.log('parallax.getBoundingClientRect()', parallax.getBoundingClientRect());
 
     // viewable parallax height
     const parallaxHeight = parallax.getBoundingClientRect().height.toFixed(0);
     // console.log('parallaxHeight', parallaxHeight);
 
-    // how much till the parallax end
-    const restOfParallax = parallaxTotalHeight - parallaxHeight;
-    // console.log('restOfParallax', restOfParallax);
-
     function changeTitlePos() {
         // how much have we scrolled
         // console.log('parallax.scrollTop', parallax.scrollTop);
 
-        const titleTransformY = (parallax.scrollTop / restOfParallax).toFixed(3) * 100 * SPEED;
-        // console.log('titleTransformY', titleTransformY);
+        const titleTransformY = (parallax.scrollTop / parallaxHeight).toFixed(3) * 100 * SPEED;
 
-        const titleOpacity = 1 - (parallax.scrollTop / restOfParallax).toFixed(1) + OPACITY;
-        // console.log('titleOpacity', titleOpacity);
+        const titleOpacity = 1 - (parallax.scrollTop / parallaxHeight).toFixed(1) + OPACITY;
 
         title.setAttribute(
             'style',
@@ -36,6 +24,15 @@ export default function () {
 
         if (parallax.scrollTop === 0) {
             title.style.opacity = 1;
+        }
+
+        let newPos = document.documentElement.clientHeight / 9.5;
+
+        if (titleTransformY.toFixed(0) >= newPos) {
+            title.setAttribute(
+                'style',
+                `transform: translateY(${newPos}%); opacity: ${titleOpacity};`
+            );
         }
     }
 
