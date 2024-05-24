721b559b6ce5abe649b02578b258a00aac99cd89
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-05-24 11:54:21 +0300

    add shine elem

---
+++ src/js/components/js_shineElem.js
@@ -0,0 +1,28 @@
+export default function () {
+    const shineContainer = document.querySelector('.js-shine-container');
+    const shineElement = document.querySelector('.js-shine-element');
+
+    shineContainer.addEventListener('mousemove', event => {
+        shineElement.classList.add('active');
+
+        // console.log('event', event);
+        // console.log('event.clientX', event.clientX);
+        // console.log('event.clientY', event.clientY);
+
+        const rect = shineContainer.getBoundingClientRect();
+
+        // console.log('rect', rect);
+        // console.log('rect.left', rect.left);
+        // console.log('rect.top', rect.top);
+
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        shineElement.style.left = `${x}px`;
+        shineElement.style.top = `${y}px`;
+    });
+
+    shineContainer.addEventListener('mouseout', () => {
+        shineElement.classList.remove('active');
+    });
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 import js_burger from './components/js_burger.js';
 import js_title from './components/js_title.js';
+import js_shineElem from './components/js_shineElem.js';
 
 (function () {
     js_burger();
     js_title();
+    js_shineElem();
 })();
