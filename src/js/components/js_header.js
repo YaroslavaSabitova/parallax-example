df6b63e7644258df51c89255629b6cacfb4bff0b
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-06-11 12:22:34 +0300

    js_header

--- src/js/components/js_header.js
+++ src/js/components/js_header.js
@@ -2,18 +2,11 @@ export default function () {
     const header = document.querySelector('.js_header');
     const parallax = document.querySelector('.js_parallax');
 
-    parallax.onscroll = function () {
+    parallax.addEventListener('scroll', function () {
         if (parallax.scrollTop > 0) {
-            // header.setAttribute(
-            //     'style',
-            //     'background-color: rgba(0, 0, 0, 0.3); backdrop-filter: blur(15px)'
-            // );
-
             header.classList.add('header-fixed');
         } else {
-            // header.removeAttribute('style');
-
             header.classList.remove('header-fixed');
         }
-    };
+    });
 }
--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,12 @@ import js_burger from './components/js_burger.js';
 import js_title from './components/js_title.js';
 import js_shineElem from './components/js_shineElem.js';
 import js_item from './components/js_item.js';
-// import js_header from './components/js_header.js';
+import js_header from './components/js_header.js';
 
 (function () {
     js_burger();
     js_title();
     js_shineElem();
     js_item();
-    // js_header();
+    js_header();
 })();
