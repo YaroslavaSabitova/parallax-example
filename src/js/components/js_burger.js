ce376e4e8ae7bbfcebad94ae9cfa1d81d30e2a43
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-07-03 12:35:44 +0300

    fixes

--- src/js/components/js_burger.js
+++ src/js/components/js_burger.js
@@ -1,14 +1,17 @@
 export default function () {
     const menu = document.querySelector('.js_menu');
     const burgerBtn = document.querySelector('.js_burger');
+    const parallax = document.querySelector('.js_parallax');
 
     burgerBtn.addEventListener('click', () => {
         menu.classList.toggle('menu-open');
 
         if (menu.classList.contains('menu-open')) {
             document.body.setAttribute('style', 'overflow: hidden');
+            parallax.setAttribute('style', 'overflow: hidden');
         } else {
             document.body.removeAttribute('style');
+            parallax.removeAttribute('style', 'overflow: hidden');
         }
     });
 }
